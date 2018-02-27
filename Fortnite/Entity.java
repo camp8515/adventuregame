0e40270a69122db32fdf167b756b546769b2db97
denizsert <30908980+denizsert@users.noreply.github.com>
2018-02-27 11:21:30 -0800

    Add files via upload

---
+++ Fortnite/Entity.java
@@ -0,0 +1,23 @@
+import java.util.*;
+/**
+ * Character.java
+ * This class describes a character.
+ * @author Deniz Sert, Nic Campanile
+ * @version February 20, 2018
+ */
+public class Entity extends NamedThing{
+    private int health;
+    private ArrayList<Item> backpack = new ArrayList<Items>();
+
+    public String getItems(){
+        return backpack.toString();
+    }
+
+    public void setHealth(int h){
+        this.health=h;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+}
