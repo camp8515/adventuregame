f27ba38587cfda87a652599701bcdc4d4a94a07a
camp8515 <30908979+camp8515@users.noreply.github.com>
2018-02-27 11:03:13 -0800

    Update NamedThing.java

--- Fortnite/NamedThing.java
+++ Fortnite/NamedThing.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-public class NamedThing {
-
-        private String name;
-        private String description;
-        
-        public String getName() {
-            return name;
-        }
-        
-        public String toString() {
-            return (name + " - " + description);
-        }
-   
-=======
-import java.util.*;
 /**
  * NamedThing.java
  * This abstract class sets up more specialized classes.
@@ -31,5 +15,5 @@ public abstract class NamedThing {
     public String toString(){
         return "Name: " + name + "\tDescription: " + description;
     }
->>>>>>> c5301ecaad930117e9fefacbd55af58824c8906f
+
 }
