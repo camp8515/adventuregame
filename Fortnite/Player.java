f65a09ad45b35aca1a9acd01de83f96a1f634370
denizsert <30908980+denizsert@users.noreply.github.com>
2018-02-27 11:17:09 -0800

    Add files via upload

---
+++ Fortnite/Player.java
@@ -0,0 +1,12 @@
+/**
+ * Player.java
+ * This class represents the perspective the user will have.
+ * @author Deniz Sert, Nic Campanile
+ * @version February 27, 2018
+ */
+public class Player{
+    public Player(){
+
+    }
+
+}
