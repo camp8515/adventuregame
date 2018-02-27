4a53ddfc95da1a2f3631f30089eae901da9ecb1b
denizsert <30908980+denizsert@users.noreply.github.com>
2018-02-27 11:14:04 -0800

    Add files via upload

---
+++ Fortnite/NPC.java
@@ -0,0 +1,13 @@
+/**
+ * NPC.java
+ * This class represents a non player character
+ * @author Deniz Sert, Nic Campanile
+ * @verison February 27, 2018
+ */
+public class NPC extends Character{
+    private int AgressiveLevel;
+    
+    public NPC(int l){
+        this.AggressiveLevel=l;
+    }
+}
