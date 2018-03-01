051dac287eaa056a687a14b9b06447bc3b09b9ed
camp8515 <30908979+camp8515@users.noreply.github.com>
2018-03-01 11:11:02 -0800

    Added Fight() Method

--- Fortnite/NPC.java
+++ Fortnite/NPC.java
@@ -5,9 +5,26 @@
  * @verison February 27, 2018
  */
 public class NPC extends Entity{
-    private int AgressiveLevel;
+    private int AggressiveLevel;
     
-    public NPC(int l){
-        this.AggressiveLevel=l;
+    public NPC(int aggression){
+        if (aggression > 10)
+            this.AggressiveLevel = 10;
+        else if (aggression < 0)
+            this.AggressiveLevel = 0;
+        else
+            this.AggressiveLevel = aggression;
     }
+    
+    public boolean fight() {
+       double results = this.AggressiveLevel * Math.random();
+       
+       if (results > 5)
+        return true;
+       else
+        return false;
+       
+    }
+        
+        
 }
