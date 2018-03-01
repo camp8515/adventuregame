61ce5a2eb1a4c176e2b86993b94c4bde0e19113c
denizsert <30908980+denizsert@users.noreply.github.com>
2018-03-01 12:17:22 -0800

    Add files via upload

---
+++ Fortnite/Weapon.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 
+ * 
+ */
+public class Weapon extends Item{
+    private int damage;
+    
+    
+    public Weapon(){
+        this.damage=(int)(Math.random()*10+1);
+    }
+    
+    
+}
