82426dce0b9edfe158465577480bb88633b4fe44
denizsert <30908980+denizsert@users.noreply.github.com>
2018-03-01 11:34:15 -0800

    fixed bug and added getFoeNum()

--- Fortnite/Place.java
+++ Fortnite/Place.java
@@ -1,3 +1,4 @@
+import java.util.*;
 /**
  * Place.java
  * This class represents the various places things can be in
@@ -5,10 +6,18 @@
  * @version February 27, 2018
  */
 public class Place extends NamedThing{
-    private ArrayList<NPC> characters = new ArrayList<NPC>();
+    private static ArrayList<NPC> characters = new ArrayList<NPC>();
     public Place(){
         for(int i = 0; i< (int)(Math.random()*5) ; i++){
-            characters.set(i) = new NPC();  
+            characters.set(i,new NPC());  
         }  
     }    
+    
+    /**
+     * Returns the number of foes in an area
+     * @return the foes in an area
+     */
+    public int getNumFoe(){
+        return characters.size();    
+    }
 }
