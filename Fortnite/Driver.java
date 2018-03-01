4d0fe1bfb486ce67440632a928eb630e4a0a1f46
denizsert <30908980+denizsert@users.noreply.github.com>
2018-03-01 10:57:55 -0800

    Add files via upload

--- Fortnite/Driver.java
+++ Fortnite/Driver.java
@@ -4,6 +4,8 @@ import java.util.*;
  * 
  */
 public class Driver{
+    private static Player p = new Player();
+    
     public static void main (String[] args){
         System.out.println("\f");
         Scanner scan = new Scanner(System.in);
@@ -13,22 +15,40 @@ public class Driver{
         System.out.println("*******************Welcome to Fornite! \n*******************");
         System.out.println("You've been kidnapped and thrown onto an island with only your pickaxe, and you " 
         + "need to survive... good luck");
-        if ((int)(Math.random()*4)) == 2){
+        if ((int)(Math.random()*4) == 2){
+            NPC foe = new NPC();
             System.out.println("An enemy has appeared. \n Run or Fight?");
             com=scan.nextLine();
-            if(com.equals("Run"){
+            if(com.equals("Run")){
                 System.out.println("You ran away.");
             }
             
-            if(com.equals("Fight"){
+            if(com.equals("Fight")){
                 System.out.println("You have the following items in your backpack");
                 for (int i=0;i<inventory.size();i++){
-                    Arrays.toString(inventory.toArray())
+                    Arrays.toString(inventory.toArray());
                 }
+                
+                foe.attack();
+                
+                
             }
+            
+            
             
         }
         
         
+        
+        
     }
+    
+    
+    public void fight(){
+        
+        
+    }
+    
+    
+  
 }
