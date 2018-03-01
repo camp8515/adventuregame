a48a75acd2bec96bb11e1448ee1df86b156419df
denizsert <30908980+denizsert@users.noreply.github.com>
2018-03-01 12:17:09 -0800

    Add files via upload

---
+++ Fortnite/Treasure.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * 
+ * 
+ */
+public class Treasure extends Item{
+    private int worth;
+    
+    
+    public Treasure(){
+       worth = (int)(Math.random()*100+1);
+    }
+    
+    
+}
